6ec9a918fa6b7a28290902ea7794ec57ec9c709f
Steve Nosse <nossesteve@gmail.com>
2020-07-05 14:39:27 +0100

    removed cloud firestore dependency

--- lib/services/users_service.dart
+++ lib/services/users_service.dart
@@ -1,12 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UsersService {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  final Firestore _db = Firestore.instance;
 
   Future<FirebaseUser> loginUser() async {
     try {
