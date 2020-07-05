420af944f245e94fd4b28a45a1af6a1d2291f2e8
Steve Nosse <nossesteve@gmail.com>
2020-07-05 21:40:23 +0100

    added some styling to the login page

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -2,6 +2,7 @@ import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:crime_map/utils/helper.dart';
 import 'package:flutter/material.dart';
 import 'package:crime_map/services/users_service.dart';
+import 'package:crime_map/widgets/footer_painter.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -28,35 +29,68 @@ class _LoginPageState extends State<LoginPage> {
       body: SafeArea(
         child: Container(
           width: size.width,
-          child: Column(
+          child: Stack(
             children: <Widget>[
-              Spacer(),
-              Text(
-                "CrimeMap",
-                style: TextStyle(
-                  fontSize: 32.0,
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).accentColor,
+              Container(
+                width: size.width,
+                height: size.height,
+                child: CustomPaint(
+                  painter: FooterPainter(),
                 ),
               ),
-              Spacer(),
-              RaisedButton.icon(
-                color: Colors.white,
-                onPressed: login,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(6.0),
-                ),
-                elevation: 2.5,
-                icon: Image.asset("assets/images/google_light.png"),
-                label: Text(
-                  "Sign in with Google",
-                  style: TextStyle(
-                    color: Colors.black45,
-                    fontWeight: FontWeight.w600,
+              Column(
+                children: <Widget>[
+                  Spacer(),
+                  Container(
+                    width: 200,
+                    height: 200,
+                    child:
+                        Image.asset("assets/images/map.png", fit: BoxFit.cover),
                   ),
-                ),
+                  RichText(
+                    text: TextSpan(
+                      text: "Crime",
+                      style: TextStyle(
+                        fontSize: 32.0,
+                        fontWeight: FontWeight.w300,
+                        color: Theme.of(context).accentColor,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: "MAP",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Container(
+                      width: size.width,
+                      child: RaisedButton.icon(
+                        color: Colors.white,
+                        onPressed: login,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6.0),
+                        ),
+                        elevation: 2,
+                        icon: Image.asset("assets/images/google_light.png"),
+                        label: Text(
+                          "Sign in with Google",
+                          style: TextStyle(
+                            color: Colors.black45,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 100),
+                ],
               ),
-              SizedBox(height: 25),
             ],
           ),
         ),
